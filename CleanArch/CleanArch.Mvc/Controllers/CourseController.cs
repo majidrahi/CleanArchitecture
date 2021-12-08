1dcff9c33e69a94263d489ea3d8a21a6efafbed4
majidrahi <majidr7091@gmail.com>
2021-12-08 12:46:01 +0300

    Presentation Course Controller and View

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model= await courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
